8da27a427a3dafe7004ffef120ce390e033fb718
Mona Lisa <divijmathur@gmail.com>
2020-01-25 22:41:31 -0800

    added styling

--- server/users.js
+++ server/users.js
@@ -6,7 +6,7 @@ const addUser = ({id, name, room}) => {
 
     const existingUser = users.find((user) => user.room === room && user.name === name);
     if(existingUser){
-        return (error:'Username is taken');
+        return {error:'Username is taken'};
     }
 
     const user = {id,name,room};
